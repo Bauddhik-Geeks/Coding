08a662b8a280e4ed2767592076184c89cff895c3
Rajkumar-stackcoder <meet.r.graphicat10@gmail.com>
2021-09-22 11:26:10 +0530

    added stdin_stdout.java

---
+++ Java/Iinterviewbit/Basic Of Java/Stdin_Stdout.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Stdin_Stdout {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+       
+        System.out.println(a);      // printing the output of a 
+        System.out.println(b);      // printing the output of b 
+    }  
+}
